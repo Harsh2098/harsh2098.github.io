bf6d3d07024677d55a9992409bbf4041df57a820
Harsh Mahajan <harshmahajan927@yahoo.com>
2020-05-09 03:20:13 +0800

    Add support for tablets

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     const progressBars = document.getElementsByClassName("progress-bar");
     setInterval(() => {
-        console.log(progressBars.size)
-        for(var i=0 ; i<progressBars.length ; ++i) {
+        console.log(progressBars.size);
+        for (var i = 0; i < progressBars.length; ++i) {
             const computedStyle = getComputedStyle(progressBars[i]);
-            const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+            const width =
+                parseFloat(computedStyle.getPropertyValue("--width")) || 0;
             progressBars[i].style.setProperty("--width", width + 0.2);
         }
     }, 5);
